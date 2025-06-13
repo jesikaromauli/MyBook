084d8e4d104447f3a60aaf67ec6aa4764bedc3fd
Jesika Romauli Aritonang <jesikaromauli4@gmail.com>
2025-06-13 10:34:25 +0700

    Mengambil data dari internet & Memproses data string

--- app/src/main/java/com/jesika0104/mybook/network/BukuApiService.kt
+++ app/src/main/java/com/jesika0104/mybook/network/BukuApiService.kt
@@ -1,23 +1,34 @@
 package com.jesika0104.mybook.network
 
+import com.jesika0104.mybook.model.Buku
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://store.sthresearch.site/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface BukuApiService {
     @GET("mybook.php")
-    suspend fun getBuku(): String
+    suspend fun getBuku(): List<Buku>
 }
 
 object BukuApi {
     val service: BukuApiService by lazy {
         retrofit.create(BukuApiService::class.java)
+    }
+
+    fun getBukuUrl(imageId: String): String {
+        return "${BASE_URL}image.php?id=$imageId"
     }
 }
